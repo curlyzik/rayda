4017559892b8340363cacff2210469d80808a821
curlyzik <curlyzik@gmail.com>
2023-03-20 20:35:53 +0100

    Updated headers

--- src/components/DashboardLayout/Header.tsx
+++ src/components/DashboardLayout/Header.tsx
@@ -3,7 +3,7 @@ import { BsBell } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <header className="flex flex-col gap-y-1">
+    <header className="flex flex-col gap-y-1 pt-4 pb-6 border-b border-[#EAECF0]">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Welcome</h1>
         <div className="relative">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
   important: true,
   theme: {
     extend: {
+      colors: {
+        raydaBlack: "#101828",
+      },
       fontFamily: {
         ibmPlex: ["IBM Plex Sans", "sans-serif"],
       },
       boxShadow: {
         auctionCardShadow:
-        // '0 35px 60px -15px rgba(0, 0, 0, 0.3)'
-          '0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)',
+          "0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)",
       },
     },
   },
