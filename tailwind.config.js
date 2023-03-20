5d23b1e43befd96e800f212a6d506277ba9c9271
curlyzik <curlyzik@gmail.com>
2023-03-20 20:11:44 +0100

    added custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  important:true,
+  important: true,
   theme: {
     extend: {
       fontFamily: {
         ibmPlex: ["IBM Plex Sans", "sans-serif"],
+      },
+      boxShadow: {
+        auctionCardShadow:
+        // '0 35px 60px -15px rgba(0, 0, 0, 0.3)'
+          '0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)',
       },
     },
   },
