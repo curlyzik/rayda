06614752cebd1bdd2500afd8370a0846587a9c2d
curlyzik <curlyzik@gmail.com>
2023-03-20 18:34:44 +0100

    added dashboard layout to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import DashboardLayout from "@/components/DashboardLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DashboardLayout>
+      <Component {...pageProps} />
+    </DashboardLayout>
+  );
 }
