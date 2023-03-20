b638da4041569ce7011fc55cc95f42fe751d936d
curlyzik <curlyzik@gmail.com>
2023-03-20 21:59:21 +0100

    installed redux toolkit

--- src/components/DashboardLayout/index.tsx
+++ src/components/DashboardLayout/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Header from "./Header";
 
 interface Props {}
-const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+const DashboardLayout: React.FC<React.PropsWithChildren<Props>> = ({
+  children,
+}) => {
   return (
-    <div className="pt-8 px-8">
+    <div className="py-8 px-8">
       <Header />
       {children}
     </div>
